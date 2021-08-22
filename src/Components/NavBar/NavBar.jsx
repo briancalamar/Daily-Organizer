600d6f05ce0783e974f8fb3fd2339f11a84954ec
briancalamar <briansantarandev@gmail.com>
2021-08-21 22:56:10 -0300

    edit routes

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,9 +4,12 @@ import { CgMenuGridO } from 'react-icons/cg'
 
 export default function NavBar(){
 
+    function handleAlert() {
+        alert("Proximamente nuevas funcionalidades!")
+    }
     return(
         <div className="navBar">
-            <CgMenuGridO className="nb-icon" />
+            <CgMenuGridO className="nb-icon" onClick={handleAlert}/>
         </div>
     )
 }
